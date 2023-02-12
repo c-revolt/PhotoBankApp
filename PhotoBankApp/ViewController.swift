f0d5206f5e292c712a236a30d09b8a0cc479de66
Alexander <a.pride.xxi@tutanota.com>
2023-02-12 13:48:55 +0300

    Initial Commit

---
+++ PhotoBankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoBankApp
+//
+//  Created by Александр Прайд on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
